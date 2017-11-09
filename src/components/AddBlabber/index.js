8be92ab7bc1bbc3d221997a68d8d6c1b094ebbda
Bridget <bridgetgathoni@gmail.com>
2017-11-09 19:47:25 +0100

    Added the AddBlabber button

---
+++ src/components/AddBlabber/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+class AddBlabber extends React.Component {
+    constructor (){
+        super()
+    }
+    render() {
+        return (
+            <div>
+                <button className='addNew'>+</button>
+            </div>
+        )
+    }
+}
+
+export default AddBlabber
