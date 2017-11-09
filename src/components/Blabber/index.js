79f0bb2e7e4517ee798fd2f69fe430f3efcfed6f
Bridget <bridgetgathoni@gmail.com>
2017-11-09 19:49:03 +0100

    added the Blabber and Blabber List component

---
+++ src/components/Blabber/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Blabber = (props) => (
+    <div>
+        <p>{props.text}</p>
+    </div>
+)
+export default Blabber
---
+++ src/components/BlabberList/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import Blabber from '../Blabber'
+
+const BlabberList = () => (
+    <ul>
+        <li>
+            <Blabber text='Blabber 1'/>
+        </li>
+        <li>
+            <Blabber text='Blabber 2'/>
+        </li>
+        <li>
+            <Blabber text='Blabber 3'/>
+        </li>
+        <li>
+            <Blabber text='Blabber 4'/>
+        </li>
+        <li>
+            <Blabber text='Blabber 5'/>
+        </li>
+    </ul>
+)
+
+export default BlabberList
