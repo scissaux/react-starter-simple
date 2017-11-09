5777915836cd508b4207e27b95e5ab24f6efb5e2
Bridget <bridgetgathoni@gmail.com>
2017-11-09 19:46:00 +0100

    Add a header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './style.css'
+
+const Header = () => (
+<header className='header'>
+    <h1 className='heading'>Orten Blabber</h1>
+    </header>)
+
+export default Header
