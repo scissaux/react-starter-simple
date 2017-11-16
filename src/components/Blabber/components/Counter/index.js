7a4ba0384e6a80bc9b67868594a6cc1d868f222f
Bridget <bridgetgathoni@gmail.com>
2017-11-16 23:20:14 +0100

    updated styling of counter

--- src/components/Blabber/components/Counter/index.js
+++ src/components/Blabber/components/Counter/index.js
@@ -4,7 +4,7 @@ import './style.css'
 
 const Counter = (props) => (
     <div>
-        <button  className='counter-plus'>+</button>
+        <button  className='counter-plus' onClick={console.log('you voted plus')} >+</button>
         <div className='votes'>{props.votes}</div>
         <button className='counter-minus'>-</button>
         
